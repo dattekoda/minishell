f47c4232fdfe0cbce0fafef56f9e68d1ef699e56
Kodai Hanadate <khanadat@K.local>
2025-10-13 00:57:41 +0900

    add: wildcard dir

---
+++ wildcard/main.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/syslimits.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <limits.h>
+#include <stdio.h>
+#include "libft.h"
+
+bool	is_wildcard(char *arg, char *file)
+{
+	if (!*arg)
+		return (!*file);
+	if (*arg == '*')
+	{
+		while (*arg == '*')
+			arg++;
+		if (!*arg)
+			return (true);
+		while (*file)
+		{
+			if (is_wildcard(arg, file))
+				return (true);
+			file++;
+		}
+		return (is_wildcard(arg, file));
+	}
+	if (*file && *arg == *file)
+		return (is_wildcard(arg + 1, file + 1));
+	return (false);
+}
+
+int	main(int argc, char *argv[])
+{
+	DIR				*dir_ptr;
+	struct dirent	*dirent_ptr;
+
+	if (argc == 1)
+		return 1;
+	if (!ft_strchr(argv[1], '*'))
+	{
+		printf("%s\n", argv[1]);
+		return 0;
+	}
+	dir_ptr = opendir(".");
+	if (!dir_ptr)
+		perror("opendir");
+	while (1)
+	{
+		dirent_ptr = readdir(dir_ptr);
+		if (!dirent_ptr)
+			break ;
+		if (*argv[1] != '.' && *dirent_ptr->d_name == '.' \
+			|| !ft_strcmp("..", dirent_ptr->d_name) \
+			|| !ft_strcmp(".", dirent_ptr->d_name))
+			continue ;
+		if (is_wildcard(argv[1], dirent_ptr->d_name))
+			printf("%s\n", dirent_ptr->d_name);
+	}
+	closedir(dir_ptr);
+	return 0;
+}
